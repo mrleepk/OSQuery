e50c8f2c236fd75abd38c7fafa1dc5ce3233fb5e
Lee Kirkpatrick <67115475+mrleepk@users.noreply.github.com>
2024-01-30 14:54:43 +0000

    Create TeamViewer_Registry_Details.sql

---
+++ Remote Monitoring and Management (RMM)/TeamViewer_Registry_Details.sql
@@ -0,0 +1,14 @@
+SELECT
+strftime('%Y-%m-%dT%H:%M:%SZ',datetime(mtime,'unixepoch')) as ModifiedTime, 
+name, 
+data,
+regex_match(path,'(S-[0-9]+(-[0-9]+)+)', '') AS sid, 
+u.username
+FROM registry
+LEFT JOIN users u ON sid = u.uuid
+WHERE
+(
+key like 'HKEY_USERS\%\SOFTWARE\TeamViewer'
+OR key like 'HKEY_CURRENT_USER\SOFTWARE\TeamViewer'
+)
+AND name IN('Meeting_UserName','CurrentSelectedLanguage')
