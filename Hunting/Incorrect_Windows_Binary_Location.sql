5de5bd557dd028dce2c627e6087d835cab3ad7dd
Lee Kirkpatrick <67115475+mrleepk@users.noreply.github.com>
2024-01-30 14:22:35 +0000

    Update Incorrect_Windows_Binary_Location.sql

--- Hunting/Incorrect_Windows_Binary_Location.sql
+++ Hunting/Incorrect_Windows_Binary_Location.sql
@@ -1,64 +1,50 @@
-SELECT * FROM processes WHERE 
+SELECT path, cmdline, pid FROM processes WHERE 
     (
-        LOWER(name)='conhost.exe' 
+        (LOWER(name)='conhost.exe' 
         AND (
             LOWER(path)!='c:\windows\system32\conhost.exe' 
-            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\conhost.exe%'
-        )
-    )
-    AND
-    (
-        LOWER(name)='dllhost.exe' 
+            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\conhost.exe'
+        ))
+        OR
+        (LOWER(name)='dllhost.exe' 
         AND (
             LOWER(path)!='c:\windows\system32\dllhost.exe' 
-            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\dllhost.exe%'
-        )
-    )
-    AND
-    (
-        LOWER(name)='lsass.exe' 
+            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\dllhost.exe'
+        ))
+        OR
+        (LOWER(name)='lsass.exe' 
         AND (
             LOWER(path)!='c:\windows\system32\lsass.exe' 
-            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\lsass.exe%'
-        )
-    )
-    AND
-    (
-        LOWER(name)='svchost.exe' 
+            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\lsass.exe'
+        ))
+        OR
+        (LOWER(name)='svchost.exe' 
         AND (
-            LOWER(path)!='c:\windows\system32\svchost.exe' 
-            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\svchost.exe%'
-        )
-    )
-    AND
-    (
-        LOWER(name)='spoolsv.exe' 
+			LOWER(path)!='c:\windows\system32\svchost.exe' 
+			AND LOWER(path) NOT LIKE 'c:\windows\syswow64\svchost.exe'
+        ))
+        OR
+        (LOWER(name)='spoolsv.exe' 
         AND (
             LOWER(path)!='c:\windows\system32\spoolsv.exe' 
-            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\spoolsv.exe%'
-        )
-    )
-    AND
-    (
-        LOWER(name)='wininit.exe' 
+            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\spoolsv.exe'
+        ))
+        OR
+        (LOWER(name)='wininit.exe' 
         AND (
             LOWER(path)!='c:\windows\system32\wininit.exe' 
-            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\wininit.exe%'
-        )
-    )
-    AND
-    (
-        LOWER(name)='winlogon.exe' 
+            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\wininit.exe'
+        ))
+        OR
+        (LOWER(name)='winlogon.exe' 
         AND (
             LOWER(path)!='c:\windows\system32\winlogon.exe' 
-            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\winlogon.exe%'
-        )
-    )
-    AND
-    (
-        LOWER(name)='wuauclt.exe' 
+            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\winlogon.exe'
+        ))
+        OR
+        (LOWER(name)='wuauclt.exe' 
         AND (
             LOWER(path)!='c:\windows\system32\wuauclt.exe' 
-            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\wuauclt.exe%'
-        )
+            AND LOWER(path) NOT LIKE 'c:\windows\syswow64\wuauclt.exe'
+        ))
     );
