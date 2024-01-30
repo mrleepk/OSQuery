2c8eef73edd5bc09f55ff585e5ce780f9b6718d8
Lee Kirkpatrick <67115475+mrleepk@users.noreply.github.com>
2024-01-30 13:53:02 +0000

    Create Installed_RMM_Tools.sql

---
+++ Remote Monitoring and Management (RMM)/Installed_RMM_Tools.sql
@@ -0,0 +1,28 @@
+SELECT 
+    name, 
+    version, 
+    install_location, 
+    publisher, 
+    install_date 
+FROM 
+    programs 
+WHERE
+    name LIKE '%anydesk%' OR
+    name LIKE '%teamviewer%' OR 
+    name LIKE '%atera%' OR
+    name LIKE '%splashtop%' OR
+    name LIKE '%ITSupport247%' OR
+    name LIKE '%NinjaRMM%' OR
+    name LIKE '%kaseya%' OR
+    name LIKE '%screenconnect%' OR 
+    name LIKE '%ConnectWise%' OR
+    name LIKE '%tacticalrmm%' OR
+    name LIKE '%NinjaOne%' OR 
+    name LIKE '%meshcentral%' OR
+    name LIKE '%ammy%' OR
+    name LIKE '%remotepc%' OR
+    name LIKE '%rustdesk%' OR
+    name LIKE '%action1%' OR
+    name LIKE '%ammyadmin%' OR
+    name LIKE '%remote utilities%' OR
+    name LIKE '%supremo%'
